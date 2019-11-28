7a51c6befd13fa58bc62192aef37ee7f044dd3fa
Jensen Van Stichel <jensen.vanstichel@hotmail.com>
2019-11-28 13:00:13 +0100

    app crashed niet meer
    door toolbar weg te doen

--- app/src/main/java/com/example/soundboard/domain/Sound.kt
+++ app/src/main/java/com/example/soundboard/domain/Sound.kt
@@ -1,16 +1,15 @@
-package com.example.soundboard
+package com.example.soundboard.domain
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
 
 @Entity(tableName = "sound_table")
 data class Sound(
     @ColumnInfo(name = "soundName")
     var naam: String = "",
     @ColumnInfo(name = "soundCategory")
-    var category: Category,
+    var category: String = "",
     @ColumnInfo(name = "soundURL")
     var URL: String = "",
     @PrimaryKey(autoGenerate = true)
